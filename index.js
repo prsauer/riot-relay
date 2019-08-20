f0cf0b4557de8402e02085f0cca6d503614f4978
Paul Sauer <armsperson@gmail.com>
2019-08-20 00:03:47 -0400

    Specify headers

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 app.use(function(req, res, next) {
   console.log(req);
   res.header("Access-Control-Allow-Origin", req.headers.origin); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "X-Riot-Token, Origin, X-Requested-With, Content-Type, Accept");
   if (req.method === 'OPTIONS') {
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
     res.send( 200 );
