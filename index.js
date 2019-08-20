4258902d51dac50a30d7be783068076f8007089b
Paul Sauer <armsperson@gmail.com>
2019-08-19 23:37:04 -0400

    test

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express.use('/', proxy('https://na1.api.riotgames.com'));
+express.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+express.use('/', proxy('https://na1.api.riotgames.com'));
