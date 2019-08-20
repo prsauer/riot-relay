d9a2753eddaa952aa6d573daccb8b9d616242bbd
Paul Sauer <armsperson@gmail.com>
2019-08-20 00:00:59 -0400

    cors mirro

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.use(function(req, res, next) {
   console.log(req);
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", req.headers.origin); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   if (req.method === 'OPTIONS') {
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
