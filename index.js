27c8e7ed33b7397b0f91a3944ee1cce5ec1f1af2
Paul Sauer <armsperson@gmail.com>
2019-08-19 23:51:50 -0400

    test

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const PORT = process.env.PORT || 5000
 
 var app = express();
 
-app.use('/', proxy('https://na1.api.riotgames.com'));
-
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  console.log(req);
+  if (req.method === 'OPTIONS') {
+    res.send( 200 );
+  } else {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  }
 });
+
+app.use('/', proxy('https://na1.api.riotgames.com'));
 
 app.listen(PORT);
