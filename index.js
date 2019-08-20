c963493cc55391c934b4b92befd6ec920e11bf01
Paul Sauer <armsperson@gmail.com>
2019-08-19 23:45:53 -0400

    TEST

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var proxy = require('express-http-proxy');
-const express = require('express')
+const express = require('express');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 var app = express();
+
+app.use('/', proxy('https://na1.api.riotgames.com'));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
@@ -11,4 +13,4 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use('/', proxy('https://na1.api.riotgames.com'));
+app.listen(PORT);
