c66c4784147021434112d64c21ceac983227ad89
Paul Sauer <armsperson@gmail.com>
2019-08-19 22:50:50 -0400

    proxy

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+var proxy = require('express-http-proxy');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+express.use('/', proxy('https://na1.api.riotgames.com'));
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
