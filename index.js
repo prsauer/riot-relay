e970a697b0b423dd5aa149c07918f21da06896db
Paul Sauer <armsperson@gmail.com>
2019-08-26 19:41:05 -0400

    Move key to ENV

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var proxy = require('express-http-proxy');
 const express = require('express');
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const RIOT_API_KEY = process.env.RIOT_API_KEY;
 
 var app = express();
 
 app.use(function(req, res, next) {
-  console.log(req);
-  res.header("Access-Control-Allow-Origin", req.headers.origin); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Headers", "X-Riot-Token, Origin, X-Requested-With, Content-Type, Accept");
-  req.headers["X-Riot-Token"] = 'RGAPI-fd81911c-fde0-4b7e-b233-4ed87f81084a';
+  req.headers["X-Riot-Token"] = RIOT_API_KEY;
   if (req.method === 'OPTIONS') {
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
     res.send( 200 );
