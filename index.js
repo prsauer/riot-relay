a7e55194ed3f4c96a8468bf957f9f71cd9a6409b
Paul Sauer <armsperson@gmail.com>
2019-08-26 21:49:44 -0400

    test cache

--- index.js
+++ index.js
@@ -12,22 +12,51 @@ const RIOT_API_KEY = process.env.RIOT_API_KEY;
 var app = express();
 var redisClient;
 
-if (process.env.REDIS_URL) {
-  ({ client: redis.createClient(process.env.REDIS_URL) })
+// process.env.REDIS_URL
+var REDIS_URL = "redis://h:pce6fb632bcd59e5435ce9b242fabe0162387173bd09544afcdcdd495bc43a6fb@ec2-18-205-197-149.compute-1.amazonaws.com:32639";
+
+if (REDIS_URL) {
+  ({ client: redis.createClient(REDIS_URL) })
 }
 
-app.use(redisClient ? redisClient.route() : (r,s,n) =>n(), function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Headers", "X-Riot-Token, Origin, X-Requested-With, Content-Type, Accept");
-  req.headers["X-Riot-Token"] = RIOT_API_KEY;
-  if (req.method === 'OPTIONS') {
-    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-    res.send( 200 );
-  } else {
-    next();
+function cacheWrite(req, res, next) {
+  if (redisClient) {
+    redisClient.add(req.originalUrl, req.body, {
+        type: res.headers['content-type'],
+        status: res.statusCode,
+      },
+    );
   }
-});
+  next();
+}
 
-app.use('/', proxy('https://na1.api.riotgames.com'));
+app.use('/',
+  function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "X-Riot-Token, Origin, X-Requested-With, Content-Type, Accept");
+    req.headers["X-Riot-Token"] = RIOT_API_KEY;
+    if (req.method === 'OPTIONS') {
+      res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+      res.send( 200 );
+    } else {
+      if (redisClient) {
+        var cached = redisClient.get(req.originalUrl);
+        if ( cache.length &&  cache[0].body != null ) {
+          res.contentType(cache[0].type);
+          res.status(cache[0].status);
+          res.send(cache[0].body);
+        } else {
+          // Cache Client but no entry
+          next();
+        }
+      } else {
+        // No Cache Client
+        next();
+      }
+    }
+  },
+  proxy('https://na1.api.riotgames.com'),
+  cacheWrite
+);
 
 app.listen(PORT);
