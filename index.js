785192c599aa19aa40355d0f7f2d34325c164736
Paul Sauer <armsperson@gmail.com>
2019-08-26 21:21:44 -0400

    Cache layer

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 var proxy = require('express-http-proxy');
+
+// Optional: Connect to a redis server to cache requests
+var cache = require('express-redis-cache');
+
 const express = require('express');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const RIOT_API_KEY = process.env.RIOT_API_KEY;
 
 var app = express();
+var redisClient;
 
-app.use(function(req, res, next) {
+if (process.env.REDIS_URL) {
+  redisClient = cache({
+    host: process.env.REDIS_URL,
+    port: 32639,
+  });
+}
+
+app.use(redisClient ? cache.route() : (r,s,n) =>n(), function(req, res, next) {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Headers", "X-Riot-Token, Origin, X-Requested-With, Content-Type, Accept");
   req.headers["X-Riot-Token"] = RIOT_API_KEY;
