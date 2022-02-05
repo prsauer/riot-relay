1cab9341d597ca9a4da01c4fb0c06ac6db96c1d9
Paul Sauer <paulrsauer@gmail.com>
2022-02-05 18:36:19 -0500

    force update

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
-var proxy = require('express-http-proxy');
+var proxy = require("express-http-proxy");
 
-var cache = require('express-redis-cache');
-var redis = require('redis');
+var cache = require("express-redis-cache");
+var redis = require("redis");
 
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 const WOW_ACCESS_TOKEN = process.env.WOW_ACCESS_TOKEN;
 
 var app = express();
+
 var redisCache;
 
 var REDIS_URL = process.env.REDIS_URL;
 
-console.log(`Proxying ${'https://us.api.blizzard.com'}`);
+console.log(`Proxying ${"https://us.api.blizzard.com"}`);
 
 if (REDIS_URL) {
   redisCache = cache({ client: redis.createClient(REDIS_URL) });
 }
 
-app.use('/',
-  function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.header('Origin'));
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(
+  "/",
+  function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.header("Origin"));
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
     req.url += `&access_token=${WOW_ACCESS_TOKEN}`;
-    if (req.method === 'OPTIONS') {
+    if (req.method === "OPTIONS") {
       res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-      res.send( 200 );
+      res.send(200);
     } else {
       if (redisCache) {
         console.log("Cache.GET", req.originalUrl);
         var cached = redisCache.get(req.originalUrl, function (error, entries) {
           console.log("Cache.HIT", entries);
-          if ( entries.length &&  entries[0].body != null ) {
+          if (entries.length && entries[0].body != null) {
             res.contentType(entries[0].type);
             res.status(200);
             res.send(entries[0].body);
@@ -48,18 +53,23 @@ app.use('/',
       }
     }
   },
-  proxy('https://us.api.blizzard.com', {
-    userResDecorator: function(proxyRes, proxyResData, userReq, userRes) {
+  proxy("https://us.api.blizzard.com", {
+    userResDecorator: function (proxyRes, proxyResData, userReq, userRes) {
       if (proxyRes.statusCode == 200 && redisCache) {
-        redisCache.add(userReq.originalUrl, proxyResData.toString('utf8'), {
-            type: proxyRes.headers['content-type'],
+        redisCache.add(
+          userReq.originalUrl,
+          proxyResData.toString("utf8"),
+          {
+            type: proxyRes.headers["content-type"],
             status: proxyRes.statusCode,
           },
-          function (error, added) { console.log("Cache.WRITE", added); }
+          function (error, added) {
+            console.log("Cache.WRITE", added);
+          }
         );
       }
       return proxyResData;
-    }
+    },
   })
 );
 
